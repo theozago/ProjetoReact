a1bc576fdd0e93f29a0562cbf776f46f15313e99
theozago <theozago@hotmail.com>
2024-11-07 11:58:35 -0300

    Telas principais

---
+++ CriarJogador.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CriarJogador = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 24 }}>Criar Jogador</Text>
+      {}
+    </View>
+  );
+};
+
+export default CriarJogador;
---
+++ TimesNBA.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TimesNBA = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 24 }}>Times da NBA</Text>
+      { }
+    </View>
+  );
+};
+
+export default TimesNBA;
